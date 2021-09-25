b45ed78f4661a784a95d7de626409f8ed14a6cf2
Julian Bolanos <julian.camilob24@gmail.com>
2021-09-25 02:23:13 -0500

    GUI implemented

--- src/ui/StoreController.java
+++ src/ui/StoreController.java
@@ -1,5 +1,6 @@
 package ui;
 
+import java.io.File;
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
@@ -7,16 +8,19 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
 import model.Store;
 
 public class StoreController {
 
+	public static final String DEFAULT_OUTPUT_PATH = "data/output/output.txt";
+	
 	private Store store;
 	
 	@FXML
-    private GridPane mainPane;
+    private VBox mainPane;
 	
 	@FXML
 	private TextField txtFileSearch;
@@ -32,11 +36,20 @@ public class StoreController {
     @FXML
     public void browse(ActionEvent event) {
 
+    	FileChooser fileChooser = new FileChooser();
+    	fileChooser.setTitle("Search input");
+    	File inputFile = fileChooser.showOpenDialog(null);
+    	if(inputFile != null) {
+    		txtFileSearch.setText(inputFile.getAbsolutePath());
+    	}
     }
 
     @FXML
-    public void importEntry(ActionEvent event) {
-
+    public void importEntry(ActionEvent event) throws IOException {
+    	store.startSection2();
+    	store.startSection4(store.startSection3());
+    	store.generateOutput(DEFAULT_OUTPUT_PATH);
+    	txtSuccessMessage.setVisible(true);
     }
     
     public void loadSelectFile() throws IOException {
