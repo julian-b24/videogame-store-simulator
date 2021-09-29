fc91a07b3317e9b005946b2ea767ab6b24231fb3
Julian Bolanos <julian.camilob24@gmail.com>
2021-09-28 19:50:21 -0500

    Boolean deleted removed from HashNode

--- src/datastructure/HashNode.java
+++ src/datastructure/HashNode.java
@@ -3,7 +3,6 @@ package datastructure;
 public class HashNode<K, V> extends Node<V>{
 
 	private K key;
-	private boolean deleted;
 
 	public HashNode(K key, V value) {
 		super(value);
@@ -16,14 +15,6 @@ public class HashNode<K, V> extends Node<V>{
 
 	public void setKey(K key) {
 		this.key = key;
-	}
-
-	public boolean isDeleted() {
-		return deleted;
-	}
-
-	public void setDeleted(boolean deleted) {
-		this.deleted = deleted;
 	}
 
 }
